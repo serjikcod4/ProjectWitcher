0527a0a23880a094593baeb2701d492ed8f2f1f3
VladKivich <vladkivich@gmail.com>
2019-05-22 23:32:50 +0300

    Camera Fix UPDATE
    Исправил ошибки.

--- Assets/Scripts/CharacterScripts/MovementController.cs
+++ Assets/Scripts/CharacterScripts/MovementController.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     private CharacterController _controller;
-    
+
     //Камера игрока
     [SerializeField] private Transform Camera;
+
     [SerializeField] private float speed = 5f;
 
     [SerializeField] private float runSpeed = 10f;
-    
+
     [SerializeField] private float rotateSpeed = 15f;
-    
+
     [SerializeField] private float mouseSensivity = 4f;
-    
+
     [SerializeField] private KeyCode runButton = KeyCode.LeftShift;
 
     [SerializeField] private float walkStaminaDrain = 0.1f;
@@ -24,18 +25,19 @@ public class MovementController : MonoBehaviour
     [SerializeField] private float runStaminaDrain = 0.3f;
 
     [SerializeField] private float gravity = -9.81f;
-    
+
+    //Флаг бега персонажа
     private bool _isRunning = false;
-    
+
     //Mock for unit data model
     private Unit unit = new Unit();
-
-
+    
     //Вектор движения персонажа.
     private Vector3 Movement = Vector3.zero;
+
     void Start()
     {
-       _controller =  gameObject.GetComponent<CharacterController>();
+        _controller = gameObject.GetComponent<CharacterController>();
     }
 
     void Update()
@@ -52,29 +54,10 @@ public class MovementController : MonoBehaviour
 
         // Check to see if the W or S key is being pressed.  
         var z = Input.GetAxis("Vertical") * (_isRunning ? runSpeed : speed);
-        
-        if (Math.Abs(z) > float.Epsilon)    
-            // Mock for Stamina Drain
-            unit.DrainStamina(_isRunning ? runStaminaDrain: walkStaminaDrain);
-        
 
-        _isRunning = Input.GetKey(runButton) && unit.CanRun;
-        
-        var x = 0f;
-   
-        // check to see if the W or S key is being pressed.  
-        var z = Input.GetAxis("Vertical") * (_isRunning ? runSpeed : speed);
-   
-        if (Math.Abs(z) > float.Epsilon)    
+        if (Math.Abs(z) > float.Epsilon)
             // Mock for Stamina Drain
-            unit.DrainStamina(_isRunning ? runStaminaDrain: walkStaminaDrain);
-        // Move the character forwards or backwards
-        _controller.SimpleMove(transform.forward * z); 
-           
-        // Check to see if the A or S key are being pressed
-        x = Input.GetAxis("Horizontal");
-
-   
+            unit.DrainStamina(_isRunning ? runStaminaDrain : walkStaminaDrain);
 
         // Check to see if the right mouse button is pressed
         if (Input.GetMouseButton(1))
@@ -118,12 +101,9 @@ public class MovementController : MonoBehaviour
 
         //Если на персонаж на поверхности, то имитируем силу тяжести.
 
- 
-        // rotate the character based on the x value
-        transform.Rotate(0, x*rotateSpeed*Time.deltaTime, 0);
-
         if (!_controller.isGrounded)
         {
-            _controller.SimpleMove(_controller.velocity +  Vector3.down*gravity * Time.deltaTime);
+            _controller.SimpleMove(_controller.velocity + Vector3.down * gravity * Time.deltaTime);
         }
+    }
 }
