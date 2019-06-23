54b8ccb04f7bbb502922c0f97d5b1f5508ea6813
VladKivich <vladkivich@gmail.com>
2019-06-23 20:56:20 +0300

    1) Добавил модель для характеристик статусов персонажа и базовых параметров персонажа 2) Перенес настройку статусов и их характеристик в EnemySpecification 3) Убрал наследование BaseConditions от ScriptableObject, оставил логику статусов в этом классе (много связей)

---
+++ Assets/Scripts/Models/ConditionsAndActions/Helpers/CharacterConditionsModel.cs
@@ -0,0 +1,37 @@
+﻿using EnemySpace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Models.ConditionsAndActions.Helpers
+{
+    public struct CharacterConditionsSpecifications
+    {
+        public float BleedingDamage { get; private set; }
+        public float BleedingTime { get; private set; }
+        public float PoisonDamage { get; private set; }
+        public float PoisonTime { get; private set; }
+        public float WeaknessDamageReduce { get; private set; }
+        public float WeaknessTime { get; private set; }
+        public float KnokedDownTime { get; private set; }
+        public float BlindingTime { get; private set; }
+        public float ImmobilizingTime { get; private set; }
+        public float SlowingTime { get; private set; }
+
+        public CharacterConditionsSpecifications(EnemySpecifications Specifications)
+        {
+            BleedingDamage = Specifications.BleedingDamage;
+            BleedingTime = Specifications.BleedingTime;
+            PoisonDamage = Specifications.PoisonDamage;
+            PoisonTime = Specifications.PoisonTime;
+            WeaknessDamageReduce = Specifications.WeaknessDamageReduce;
+            WeaknessTime = Specifications.WeaknessTime;
+            KnokedDownTime = Specifications.KnokedDownTime;
+            BlindingTime = Specifications.BlindingTime;
+            ImmobilizingTime = Specifications.ImmobilizingTime;
+            SlowingTime = Specifications.SlowingTime;
+        }
+    }
+}
